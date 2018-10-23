22de90848d53937ed51fb3591849841f1133818c
Chloe Chong <chloechong.us@gmail.com>
2018-10-23 17:13:14 -0400

    Updated server database name

--- server/db/database.js
+++ server/db/database.js
@@ -2,6 +2,8 @@
 
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(`postgres://localhost:5432/handyquiz`, { logging: false });
+const dbName = 'replace with your database name';
+
+const db = new Sequelize(`postgres://localhost:5432/${dbName}`, { logging: false });
 
 module.exports = db;
