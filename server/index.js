19c73ccf1115a1a08b0c5d3efeec69d22fe89d29
Chloe Chong <chloechong.us@gmail.com>
2018-10-23 16:04:10 -0400

    Initialized server with database

--- server/index.js
+++ server/index.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const express = require('express');
+const path = require('path');
+const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// logging middleware
+app.use(volleyball);
+
+// body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// static middleware
+app.use(express.static(path.join(__dirname, '../public')));
+
+// all routes
+app.use('/api', require('./api'));
+
+// Send index.html for any other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
+
+module.exports = app;
