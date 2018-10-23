0ced3d49ce7a8cab2a2d3aaa73e542351d9c6e38
Chloe Chong <chloechong.us@gmail.com>
2018-10-23 17:26:37 -0400

    Updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
   entry: ['@babel/polyfill', './client/index.js'],
   output: {
     path: __dirname,
     filename: './public/bundle.js',
   },
-  mode: 'development',
+  mode: isDev ? 'development' : 'production',
   devtool: 'source-map',
   module: {
     rules: [
