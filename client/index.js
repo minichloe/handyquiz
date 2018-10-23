447b4b9a935424a080f7d5309a365f04df721113
Chloe Chong <chloechong.us@gmail.com>
2018-10-23 16:47:06 -0400

    Added basic react page

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app';
+import App from './App';
 
-ReactDom.render(<App />, document.getElementById('main'));
+ReactDOM.render(<App />, document.getElementById('main'));
