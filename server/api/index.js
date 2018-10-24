44b03240d05ae97e76b59a5d78d90fa3e25386f4
Chloe Chong <chloechong.us@gmail.com>
2018-10-24 13:00:12 -0400

    Added api routes to server

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,10 @@
 
 const router = require('express').Router();
 
+router.use('/professional', require('./professional'));
+router.use('./testCleaning', require('./testCleaning'));
+router.use('./testHandyman', require('./testHandyman'));
+
 router.use((req, res, next) => {
   const err = new Error('API endpoint not found');
   err.status = 404;
