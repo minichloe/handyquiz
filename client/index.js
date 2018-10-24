2693a2d3e8ec767d5bd2f34bf6084e9df83a6067
Chloe Chong <chloechong.us@gmail.com>
2018-10-24 13:08:03 -0400

    Updated to include provider and routes

--- client/index.js
+++ client/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import store from './store';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('main')
+);
