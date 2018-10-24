796ed6ca20fa0a2d230d13f6a167b5b70e2178e9
Chloe Chong <chloechong.us@gmail.com>
2018-10-24 13:33:16 -0400

    Added store and reducer

---
+++ client/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import reducer from './reducer';
+
+const middleware = applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }));
+const store = createStore(reducer, middleware);
+
+export default store;
---
+++ client/store/reducer.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+
+const GOT_PROFESSIONALS = 'GOT_PROFESSIONALS';
+const GOT_TEST = 'GOT_TEST';
+const GOT_NEW_PROFESSIONAL = 'GOT_NEW_PROFESSIONAL';
+
+const initialState = {
+  professionals: [],
+  admin: false,
+  currApplicant: {},
+  test: {},
+};
+
+const gotProfessionals = professionals => ({ type: GOT_PROFESSIONALS, professionals });
+
+const gotTest = test => ({ type: GOT_TEST, test });
+
+const createdApplicant = currApplicant => ({ type: GOT_NEW_PROFESSIONAL, currApplicant });
+
+export const getProfessionals = () => async dispatch => {
+  try {
+    const { data } = await axios.get('/api/professional');
+    dispatch(gotProfessionals(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getTest = test => async dispatch => {
+  const testType = test.type;
+  delete test.type;
+  const { data } = await axios.post(`/api/${testType}`, test);
+  dispatch(gotTest(data));
+};
+
+export const createApplicant = professional => async dispatch => {
+  const { data } = await axios.post('/api/professional', professional);
+  dispatch(createdAplicant(data));
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GOT_PROFESSIONALS:
+      return {
+        ...state,
+        professionals: action.professionals,
+      };
+    case GOT_TEST:
+      return {
+        ...state,
+        test: action.test,
+      };
+    case GOT_NEW_PROFESSIONAL:
+      return {
+        ...state,
+        currApplicant: action.currApplicant,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
