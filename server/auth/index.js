dc04713db0230072d77480d621dfc559a4c6ac4a
Chloe Chong <chloechong.us@gmail.com>
2018-10-28 20:26:45 -0400

    Added admin routes

---
+++ server/auth/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const router = require('express').Router();
+
+router.put('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = User.findOne({
+      where: {
+        email,
+        password,
+      },
+    });
+    if (user) {
+      req.session.userId = user.id;
+      res.json(user);
+    } else {
+      res.status(401).send();
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/me', async (req, res, next) => {
+  try {
+    if (req.session.userId) {
+      const user = await User.findOne({ where: { id: req.session.userId } });
+      res.json(user);
+    } else {
+      res.status(404).send();
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/logout', (req, res, next) => {
+  req.session.destroy();
+  res.status(204).end();
+});
+
+router.use((req, res, next) => {
+  const err = new Error('API endpoint not found');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // all routes
 app.use('/api', require('./api'));
+app.use('/auth', require('./auth'));
 
 // static middleware
 app.use(express.static(path.join(__dirname, '..', 'public')));
